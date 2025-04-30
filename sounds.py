0a6a5034fa944f9738b67028dd4bbd015eea34d2
hazel410 <wild.hazelnut.tree@gmail.com>
2025-04-30 15:17:54 -0400

    began pygbag migration

--- sounds.py
+++ sounds.py
@@ -1,15 +1,21 @@
 import pygame
 from constants import MP3_LOCATION
+from constants import RUNNING_LOCALLY
 
-pygame.mixer.music.load(MP3_LOCATION + 'HGSS - Pewter City.mp3')
+if RUNNING_LOCALLY:
+  file_extension = '.mp3'
+else:
+  file_extension = '.ogg'
+
+pygame.mixer.music.load(MP3_LOCATION + 'HGSS - Pewter City' + file_extension)
 pygame.mixer.music.set_volume(0.1)
 pygame.mixer.music.play(loops=-1)
-pop = pygame.mixer.Sound(MP3_LOCATION + 'pop.mp3')
-yip = pygame.mixer.Sound(MP3_LOCATION + 'yippee.mp3')
-bye = pygame.mixer.Sound(MP3_LOCATION + 'roblox-bye.mp3')
-che = pygame.mixer.Sound(MP3_LOCATION + 'children-cheering.mp3')
-cor = pygame.mixer.Sound(MP3_LOCATION + 'correct.mp3')
-pip = pygame.mixer.Sound(MP3_LOCATION + 'metal-pipe-clang.mp3')
+pop = pygame.mixer.Sound(MP3_LOCATION + 'pop' + file_extension)
+yip = pygame.mixer.Sound(MP3_LOCATION + 'yippee' + file_extension)
+bye = pygame.mixer.Sound(MP3_LOCATION + 'roblox-bye' + file_extension)
+che = pygame.mixer.Sound(MP3_LOCATION + 'children-cheering' + file_extension)
+cor = pygame.mixer.Sound(MP3_LOCATION + 'correct' + file_extension)
+pip = pygame.mixer.Sound(MP3_LOCATION + 'metal-pipe-clang' + file_extension)
 pop.set_volume(.1)
 yip.set_volume(.1)
 bye.set_volume(.1)
